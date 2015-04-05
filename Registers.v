e1f43a97557ff11875230be461bf952efe1132b4
LeftRadio <wladkam@mail.com>
2015-04-05 19:29:19 +0300

    sycn mod, schematic.

--- Registers.v
+++ Registers.v
@@ -13,10 +13,10 @@
 `define extPin_reg_0_A   (5'b01011)
 `define extPin_reg_1_A   (5'b01100)
 `define Write_Control_A  (5'b01101)
-//`define BackLight_A      (5'b01110)
 `define SRAM_DATA        (5'b01111)
-//`define TRIG_MASK_A      (5'b10000)
 `define cnfPin_B     	 (5'b10000)
+`define LA_MASK_COND  	 (5'b10001)
+`define LA_MASK_DIFF   	 (5'b10010)
 
 module Registers
 (
@@ -32,7 +32,9 @@ output reg [7:0]    REG_DATA_OUT,
 output reg [23:0]   Decimation,
 output reg [7:0]    Trigger_level_UP,
 output reg [7:0]    Trigger_level_Down,
-//output reg [7:0]    TRIG_MASK_reg,
+output reg [7:0]    LA_TriggerMask_Cond,
+output reg [7:0]    LA_TriggerMask_Diff,
+
 output reg [17:0]   WIN_DATA,
 output reg [7:0]    Delay,
 output reg          Start_Write_s,
@@ -98,7 +100,6 @@ reg [3:0]    extPin_reg_0;
 reg [7:0]    extPin_reg_1;
 
 
-
 reg STR, ENTr, INT_0, INT_1;
 
 
@@ -114,21 +115,22 @@ always @(posedge Write) begin
   else if(Addr_or_Data == 0) begin
        
 	   case (Sel_Addr_reg)
-	   `Decim_Low        :	 Decimation[7:0]     <=  DATA_IN;
-	   `Decim_Height0    :	 Decimation[15:8]    <=  DATA_IN;
-	   `Decim_Height1    :	 Decimation[23:16]   <=  DATA_IN;
-	   `Trigger_UP       :	 Trigger_level_UP    <=  DATA_IN;
-       `Trigger_Down     :	 Trigger_level_Down  <=  DATA_IN;
-	   `WIN_DATA_Low     :	 WIN_DATA[7:0]       <=  DATA_IN;
-	   `WIN_DATA_Height0 :	 WIN_DATA[15:8]      <=  DATA_IN;
-	   `WIN_DATA_Height1 :	 WIN_DATA[17:16]     <=  DATA_IN[1:0];
-	   `cnfPin_A         :	 cnfPin              <=  DATA_IN[7:0];
-	   `Del              :	 Delay               <=  DATA_IN;
-	   `extPin_reg_0_A   :   extPin_reg_0        <=  DATA_IN[3:0];
-	   `extPin_reg_1_A   :   extPin_reg_1        <=  DATA_IN[7:0];
-	   `Write_Control_A  :   Write_Control       <=  DATA_IN[1:0];
-	   //`TRIG_MASK_A      :   TRIG_MASK_reg       <=  DATA_IN;
-	   `cnfPin_B  		 :   cnfPinB		     <=  DATA_IN[2:0];
+	   `Decim_Low        : Decimation[7:0]     <=  DATA_IN;
+	   `Decim_Height0    : Decimation[15:8]    <=  DATA_IN;
+	   `Decim_Height1    : Decimation[23:16]   <=  DATA_IN;
+	   `Trigger_UP       : Trigger_level_UP    <=  DATA_IN;
+       `Trigger_Down     : Trigger_level_Down  <=  DATA_IN;
+	   `WIN_DATA_Low     : WIN_DATA[7:0]       <=  DATA_IN;
+	   `WIN_DATA_Height0 : WIN_DATA[15:8]      <=  DATA_IN;
+	   `WIN_DATA_Height1 : WIN_DATA[17:16]     <=  DATA_IN[1:0];
+	   `cnfPin_A         : cnfPin              <=  DATA_IN;
+	   `Del              : Delay               <=  DATA_IN;
+	   `extPin_reg_0_A   : extPin_reg_0        <=  DATA_IN[3:0];
+	   `extPin_reg_1_A   : extPin_reg_1        <=  DATA_IN;
+	   `Write_Control_A  : Write_Control       <=  DATA_IN[1:0];
+	   `cnfPin_B  		 : cnfPinB		       <=  DATA_IN[2:0];
+	   `LA_MASK_COND	 : LA_TriggerMask_Cond <=  DATA_IN;
+	   `LA_MASK_DIFF	 : LA_TriggerMask_Diff <=  DATA_IN;
 	   default;		 
 	   endcase
 	   
@@ -154,8 +156,9 @@ always @* begin	//
 		`extPin_reg_1_A   :	 REG_DATA_OUT = extPin_reg_1;
 		`Write_Control_A  :	 REG_DATA_OUT = Write_Control;
 		`SRAM_DATA        :	 REG_DATA_OUT = SRAM_TO_MCU_DATA;
-		//`TRIG_MASK_A      :  REG_DATA_OUT = TRIG_MASK_reg;
 		`cnfPin_B         :	 REG_DATA_OUT = cnfPinB;
+		`LA_MASK_COND	  :  REG_DATA_OUT = LA_TriggerMask_Cond;
+	    `LA_MASK_DIFF	  :  REG_DATA_OUT = LA_TriggerMask_Diff;
 		default           :  REG_DATA_OUT = 0;
   endcase
 end	//always
